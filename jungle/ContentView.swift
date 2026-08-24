1c793a30f3d218714c44943d4d56fedffb4bdbe7
Antonio Orozco <orozco6453@gmail.com>
2026-08-24 10:53:00 -0700

    Initial Commit

---
+++ jungle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jungle
+//
+//  Created by Antonio Orozco on 8/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jungle/jungleApp.swift
@@ -0,0 +1,17 @@
+//
+//  jungleApp.swift
+//  jungle
+//
+//  Created by Antonio Orozco on 8/24/26.
+//
+
+import SwiftUI
+
+@main
+struct jungleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
